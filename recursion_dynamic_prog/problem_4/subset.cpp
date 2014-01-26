2c0c3bb4f9af7bf6a7822b41a9dcb86d3ed87506
Chenthill Palanisamy <chenthillrulz@gmail.com>
2014-01-26 16:17:45 +0530

    Fix issues in the subset problem. Make sure duplicate sets are removed from the subsets generated

--- recursion_dynamic_prog/problem_4/subset.cpp
+++ recursion_dynamic_prog/problem_4/subset.cpp
@@ -7,11 +7,27 @@ using namespace std;
 
 typedef list<shared_ptr<set<int>>> setList;
 
-
 bool
 is_set_equal (shared_ptr<set<int>> a, shared_ptr<set<int>> b)
 {
-	return (*a == *b);
+	bool isEqual = (*a == *b);
+//	cout << "Set is equal " << isEqual << endl;
+	return isEqual;
+}
+
+bool
+list_compare_func (shared_ptr<set<int>> a, shared_ptr<set<int>> b)
+{
+	return *a < *b;	
+}
+
+bool
+has_item_in_set (shared_ptr<set<int>> sptrSet, int val)
+{
+	set<int>::iterator iter;
+
+	iter = sptrSet->find (val);
+	return (iter != sptrSet->end());
 }
 
 static setList
@@ -23,16 +39,20 @@ generate_sets (setList appendTo, shared_ptr< list<int> > individualItems)
 	{
 		for (auto j : appendTo)
 		{
-			shared_ptr<set<int>> subset (new set <int> (*j));
-			subset->insert (i);
-			result.push_back (subset);
+			if (has_item_in_set (j, i) == false)
+			{
+				shared_ptr<set<int>> subset (new set <int> (*j));
+				subset->insert (i);
+				result.push_back (subset);
+			}
 		}
 	}
 
-	// remove duplicates from result
+	// remove duplicate sets from result
+	result.sort (list_compare_func);
 	result.unique (is_set_equal);
 
-	cout << "Number of subsets generated " << result.size () << endl;
+	cout << "Number of subsets generated in current recursive call " << result.size () << endl;
 
 	if (result.size () != 1)
 	{
@@ -46,12 +66,18 @@ generate_sets (setList appendTo, shared_ptr< list<int> > individualItems)
 int 
 main ()
 {
-	setList result;
+	setList result, resultToAdd;
 	shared_ptr< set<int> > initialSet (new set<int> ());
 	shared_ptr< list<int> > individualItems (new list<int> ());
 
+	cout << "Problem: Generate all subsets for set { ";
 	for (int i=5; i < 9; i ++)
+	{
+		cout << i << " ";	
 		initialSet->insert (i);
+	}
+	cout << "}" << endl;
+
 
 	for (auto i : *initialSet)
 	{
@@ -62,8 +88,19 @@ main ()
 		result.push_back (subset);
 	}
 
-	result = generate_sets (result, individualItems);
-
+	resultToAdd = generate_sets (result, individualItems);
+	result.merge (resultToAdd);
+	
+	cout << "subsets generated " << endl;
+	for (auto i : result)
+	{
+		cout << "Subset elements " << endl;
+		for (auto j : *i)
+		{
+			cout << " " << j;
+		}
+		cout << endl;
+	}
 	cout << "Total number of subsets generated " << result.size () << endl;
 
 	return 0;
